dc4e55b15a334c6102417dd0586382a212887893
natan <natan.volkovich@flexshopper.com>
2015-04-28 16:07:00 -0400

    Updated price.js to use v1 css

--- src/price.js
+++ src/price.js
@@ -144,7 +144,7 @@ var linkCss = function () {
     css.type = 'text/css';
     css.rel = 'stylesheet';
     css.async = true;
-    css.href = (document.location.protocol == 'https:' ? 'https://cdn' : 'http://cdn') + '.flexprice.com/flexprice.css';
+    css.href = (document.location.protocol == 'https:' ? 'https://cdn' : 'http://cdn') + '.flexprice.com/v1/flexprice.css';
 
     if (debug) {
         // css.href = 'http://plugin.flexshopper.dev/flexprice.css'; // debug
